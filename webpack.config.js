61b0a7733039f2ea90938e84ecbb25751faf60a2
chikui <2099027339@qq.com>
2021-01-06 11:25:12 +0800

    node_modules

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './lib/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'gulu-ui',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'awesome-typescript-loader' }
+    ]
+  }
+}
