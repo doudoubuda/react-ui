57be3394fe52c45a70c99f35a0deddef3ced5a6a
chikui <2099027339@qq.com>
2021-01-07 11:19:22 +0800

    安装了react react-dom!

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,5 +13,9 @@ module.exports = {
     rules: [
       { test: /\.tsx?$/, use: 'awesome-typescript-loader' }
     ]
-  }
+  },
+  plugins: [...base.plugins, new HtmlWebpackPlugin({
+    title: 'GULU - React',
+    template: 'index.html'
+  })],
 }
